d555900b316e6fb62cd88c5281923a971a318c05
fkuloff <kul.artem@bk.ru>
2021-09-23 14:03:59 +0300

    new app category

--- DjangoEcommerce/urls.py
+++ DjangoEcommerce/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('greatkart.urls')),
+    path('', include('apps.greatkart.urls')),
 ]
