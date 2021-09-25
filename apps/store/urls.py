aa04d9066bbf56edf59e22ca89e3673fd8914c01
fkuloff <kul.artem@bk.ru>
2021-09-25 23:01:45 +0300

    category

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from .views import store
 
 urlpatterns = [
-    path('', store, name='homepage'),
+    path('', store, name='store'),
+    path('<slug:category_slug>', store, name='products_by_category'),
 ]
