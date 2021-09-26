8834d0e8574a19d133261cc1be36bbc4261bcf1b
fkuloff <kul.artem@bk.ru>
2021-09-26 12:37:10 +0300

    add cart app

--- DjangoEcommerce/urls.py
+++ DjangoEcommerce/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('apps.greatkart.urls')),
     path('store/', include('apps.store.urls')),
+    path('cart/', include('apps.cart.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
