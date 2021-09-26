77264d12d29e147bf57ddbe991b9640d8f2d3975
fkuloff <kul.artem@bk.ru>
2021-09-26 13:47:41 +0300

    add to cart and cart view upgrade

--- apps/cart/urls.py
+++ apps/cart/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import cart
+from .views import cart, add_cart
 
 urlpatterns = [
-    path('', cart, name='cart')
+    path('', cart, name='cart'),
+    path('add/<int:product_id>', add_cart, name='add_cart')
 ]
