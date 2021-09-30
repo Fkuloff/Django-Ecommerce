5a452db609c5d0dfead1918bfdfc8f6c07acaf5e
fkuloff <kul.artem@bk.ru>
2021-09-30 15:16:38 +0300

    Registration

--- DjangoEcommerce/urls.py
+++ DjangoEcommerce/urls.py
@@ -4,8 +4,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('apps.greatkart.urls')),
-    path('store/', include('apps.store.urls')),
-    path('cart/', include('apps.cart.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', include('apps.greatkart.urls')),
+                  path('store/', include('apps.store.urls')),
+                  path('cart/', include('apps.cart.urls')),
+                  path('accounts/', include('apps.accounts.urls')),
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ apps/accounts/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from .models import Account
+
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter Password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm Password'
+    }))
+
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def clean(self):
+        cleaned_date = super(RegistrationForm, self).clean()
+        password = cleaned_date.get('password')
+        confirm_password = cleaned_date.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password does not match'
+            )
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
