810cad0502ff23d936b1a9ee7f33803615fce7a7
fkuloff <kul.artem@bk.ru>
2021-10-02 14:57:48 +0300

    Forgot Password Reset Password Validation

--- DjangoEcommerce/settings.py
+++ DjangoEcommerce/settings.py
@@ -146,5 +146,5 @@ MESSAGE_TAGS = {
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'kuleshovartemolegovich@gmail.com'
+EMAIL_HOST_PASSWORD = 'qgOJH0MzSP'
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,11 +1,16 @@
 from django.urls import path, include
-from .views import register, login, logout, activate, dashboard
+from .views import register, login, logout, activate, dashboard, forgotPassword, resetpassword_validate, resetPassword
 
 urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
-    path('activate/<uidb64>/<token>', activate, name='activate'),
     path('dashboard/', dashboard, name='dashboard'),
     path('', dashboard, name='dashboard'),
+
+    path('activate/<uidb64>/<token>', activate, name='activate'),
+    path('resetpassword_validate/<uidb64>/<token>', resetpassword_validate, name='resetpassword_validate'),
+    path('forgotPassword/', forgotPassword, name='forgotPassword'),
+    path('resetPassword/', resetPassword, name='resetPassword'),
+
 ]
