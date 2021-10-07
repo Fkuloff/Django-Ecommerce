cbaf3ca24041d6faf32d734722147e7300f8fc5c
fkuloff <kul.artem@bk.ru>
2021-10-07 13:17:45 +0300

    Order payment page

--- apps/order/admin.py
+++ apps/order/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models import Payment, Order, OrderProduct
 
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('order_number', 'user', 'created_at', 'status', 'is_ordered')
+
+
 admin.site.register(Payment)
-admin.site.register(Order)
+admin.site.register(Order, OrderAdmin)
 admin.site.register(OrderProduct)
