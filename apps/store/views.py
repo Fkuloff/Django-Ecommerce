b770abd18d8968d829c32c895b2320b542fff44a
fkuloff <kul.artem@bk.ru>
2021-10-11 14:11:12 +0300

    display reviews and ratings stars

--- apps/store/views.py
+++ apps/store/views.py
@@ -51,10 +51,14 @@ def product_detail(request, category_slug, product_slug):
     except OrderProduct.DoesNotExist:
         order_product = None
 
+    # get reviews
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'order_product': order_product,
+        'reviews': reviews,
     }
 
     return render(request, 'store/product_detail.html', context)
