5cb738b69309f460a9212cda31c6b16f1d0bc370
fkuloff <kul.artem@bk.ru>
2021-10-14 13:58:19 +0300

    update rating stars in home/store

--- apps/greatkart/views.py
+++ apps/greatkart/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from apps.store.models import Product
+from apps.store.models import ReviewRating
 
 
 def home(request):
-    products = Product.objects.filter(is_available=True)
+    products = Product.objects.filter(is_available=True).order_by('created_date')
+
+    for p in products:
+        print(p.average_review)
+
     context = {
         'products': products,
     }
