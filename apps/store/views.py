8f2a89b3633cd303b839c1fa2adfb5523465d25e
fkuloff <kul.artem@bk.ru>
2021-10-14 13:38:36 +0300

    product gallery

--- apps/store/views.py
+++ apps/store/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Product, ReviewRating
+from .models import Product, ReviewRating, ProductGallery
 from apps.category.models import Category
 from apps.cart.models import CartItem
 from apps.cart.views import _cart_id
@@ -57,11 +57,14 @@ def product_detail(request, category_slug, product_slug):
     # get reviews
     reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
 
+    product_gallery = ProductGallery.objects.filter(product_id=single_product.id)
+
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
         'order_product': order_product,
         'reviews': reviews,
+        'product_gallery': product_gallery,
     }
 
     return render(request, 'store/product_detail.html', context)
