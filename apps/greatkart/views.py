35b15b714906fa420ba3fbb46f5ac015350c8fb8
fkuloff <kul.artem@bk.ru>
2021-10-16 18:52:14 +0300

    fix footer fix index fix product detail

--- apps/greatkart/views.py
+++ apps/greatkart/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from apps.store.models import Product, Variation
+from apps.store.models import Product, Variation, ProductGallery
 from apps.store.models import ReviewRating
 
 
 def home(request):
     products = Product.objects.filter(is_available=True).order_by('created_date')
 
+    variations = []
+    for p in products:
+        variation = Variation.objects.filter(product=p).first()
+        variations.append(variation)
+
+    # for i, b in list_1, list_2:
+
     context = {
-        'products': products,
+        # 'products': products,
+        'variations': variations,
     }
-    return render(request, 'home.html', context)
+    return render(request, 'index.html', context)
