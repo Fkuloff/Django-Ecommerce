098f8958e307a1588e890a290e396a3fa13d6825
fkuloff <kul.artem@bk.ru>
2021-11-15 18:33:55 +0300

    fix bug with img/ delete checkout.html

--- apps/order/views.py
+++ apps/order/views.py
@@ -81,7 +81,7 @@ def payments(request):
 
     try:
         mail_subject = 'Thank you for your order!'
-        message = render_to_string('orders/order_received_email.html', {
+        message = render_to_string('email/order_received_email.html', {
             'user': request.user,
             'order': order,
         })
